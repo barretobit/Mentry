b1788893bd421044c496d5d8ebb0c2e69f576f6f
João Barreto <barretobit@gmail.com>
2025-05-11 22:52:51 +0200

    Adding the FocusEntry Model and MainWindows adjustments;

---
+++ Mentry/Models/FocusEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace Mentry.Models;
+
+public class FocusEntry
+{
+    public DateTime Date { get; set; } = DateTime.Today;
+    public List<FocusTask> Tasks { get; set; } = new();
+}
+
+public class FocusTask
+{
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
