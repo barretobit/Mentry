97ae20144fa301feeca97b087e530f30747e0361
João Barreto <barretobit@gmail.com>
2025-05-12 22:53:24 +0200

    Configuration of the MainViewModel;

---
+++ Mentry/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mentry.Models;
+
+namespace Mentry.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string newTaskDescription = string.Empty;
+
+    public ObservableCollection<FocusTask> TodayTasks { get; } = new();
+
+    [RelayCommand]
+    private void AddTask()
+    {
+        if (!string.IsNullOrWhiteSpace(NewTaskDescription) && TodayTasks.Count < 3)
+        {
+            TodayTasks.Add(new FocusTask { Description = NewTaskDescription });
+            NewTaskDescription = string.Empty;
+        }
+    }
+
+    [RelayCommand]
+    private void ToggleComplete(FocusTask task)
+    {
+        task.IsCompleted = !task.IsCompleted;
+        OnPropertyChanged(nameof(TodayTasks));
+    }
+}
