89556552b39bc82985e8cf4b595956e5a4c9e4eb
João Barreto <barretobit@gmail.com>
2025-05-13 23:04:21 +0200

    Modifications in the App.xaml.cs to include MainViewModel;

--- Mentry/App.xaml.cs
+++ Mentry/App.xaml.cs
@@ -1,14 +1,37 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Mentry.Services;
+using Mentry.ViewModels;
+using Mentry.Views;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Mentry
+namespace Mentry;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public new static App Current => (App)Application.Current;
+    public IServiceProvider Services { get; private set; }
+
+    public App()
     {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        Services = services.BuildServiceProvider();
     }
 
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IStorageService, JsonStorageService>();
+        services.AddTransient<MainViewModel>();
+        services.AddTransient<MainWindow>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainWindow = Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+        base.OnStartup(e);
+    }
 }
